6f085515cd612665cb61c8690c8cb56227944ed7
apahie <apahie@icloud.com>
2019-05-19 22:19:42 +0900

    add sample variables and function

--- warkthrough-haskell/first-step/main.hs
+++ warkthrough-haskell/first-step/main.hs
@@ -1,7 +1,11 @@
+m = 123
+n = 456
+add x y = x + y
 -- this is line comment
 main = do
   putStrLn "hello, world"　-- this is line comment too
   putStrLn "goodbye"
+  print(add m n)
 {- block comment can
   {- be nested -}
 -}
