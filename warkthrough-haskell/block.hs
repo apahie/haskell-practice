409940f33b37c824fe0642ed4424f2d32820f17a
apahie <apahie@icloud.com>
2019-05-19 23:10:13 +0900

    ファイル統合時の元ファイルを削除

--- warkthrough-haskell/block.hs
+++
@@ -1,4 +0,0 @@
-main = do
-  putStrLn "hello"
-  putStrLn "world"
-  putStrLn "goodbye"
--- warkthrough-haskell/hello.hs
+++
@@ -1,6 +0,0 @@
--- this is line comment
-main = putStrLn "hello, world"　-- this is line comment too
-
-{- block comment can
-  {- be nested -}
--}
