0fb2258eb8334b729adfc2287bfde5626124577f
apahie <apahie@icloud.com>
2019-05-25 17:13:07 +0900

    pattern sample

--- warkthrough-haskell/expressions/main.hs
+++ warkthrough-haskell/expressions/main.hs
@@ -1,6 +1,11 @@
 main = do print m
           print n
+          print a
+          print b
+          -- print _
+          print t
 
 m, n :: Int
 m = 1 + 3
 n = 456
+t@(a, b, _, _) = (1, 3.14, "hello", 3)
