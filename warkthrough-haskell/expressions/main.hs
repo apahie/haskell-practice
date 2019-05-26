bf20eab73d02b45a8a2756a7945be5af03e520b5
apahie <apahie@icloud.com>
2019-05-26 11:50:18 +0900

    pattern matching sample

--- warkthrough-haskell/expressions/main.hs
+++ warkthrough-haskell/expressions/main.hs
@@ -4,8 +4,16 @@ main = do print m
           print b
           -- print _
           print t
+          print(getValue 0 Nothing)
+          print(getValue 0 (Just 5))
 
 m, n :: Int
 m = 1 + 3
 n = 456
 t@(a, b, _, _) = (1, 3.14, "hello", 3)
+
+getValue defval maybe =
+  case maybe of
+    Nothing -> defval
+    -- _ -> defval
+    Just x -> x
