50c73fbd2d535afceaa1636d81e390e9cf22e647
apahie <apahie@icloud.com>
2019-05-29 00:28:27 +0900

    add recursive function sample

--- warkthrough-haskell/functions.hs
+++ warkthrough-haskell/functions.hs
@@ -1,9 +1,15 @@
 add :: Int -> Int -> Int
 add x y = x + y
 
+factorial :: Int -> Int
+factorial x =
+  if x == 1 then 1
+  else x * factorial (x - 1)
+
 main :: IO()
 main = do
   print (add 2 3)
   print (add 1 2 * 3) -- 9が表示(関数適用は中置演算子より優先度が高い)
   print (add 1 (2 * 3)) -- 7が表示
   print $ add 1 $ 2 * 3 -- 上と同じ
+  print $ factorial 5
