8d02773ce019dcb06a5e57f1d2917f63e56400c1
apahie <apahie@icloud.com>
2019-05-29 00:13:58 +0900

    add priority sample

--- warkthrough-haskell/functions.hs
+++ warkthrough-haskell/functions.hs
@@ -1,2 +1,6 @@
 add x y = x + y
-main = print (add 2 3)
+main = do
+  print (add 2 3)
+  print (add 1 2 * 3) -- 9が表示(関数適用は中置演算子より優先度が高い)
+  print (add 1 (2 * 3)) -- 7が表示
+  print $ add 1 $ 2 * 3 -- 上と同じ
