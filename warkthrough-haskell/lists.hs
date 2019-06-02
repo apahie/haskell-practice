07a16c024beec97160fdd462442697568d1257de
apahie <apahie@icloud.com>
2019-06-02 18:51:49 +0900

    add list functions sample

--- warkthrough-haskell/lists.hs
+++ warkthrough-haskell/lists.hs
@@ -1,6 +1,18 @@
+import Prelude hiding (map)
+
 char = ['a', 'b', 'c']
-str = "abc" -- [Char] 型（）
+str = "abc" -- [Char] 型（charと同じ型）
+sample = [1, 2, 3]
+
+map :: (a -> b) -> [a] -> [b]
+map _ [] = []
+map f (x:xs) = f x : map f xs
 
 main = do
   print $ char
   print $ str
+  print $ head sample
+  print $ tail sample
+  print $ [1] ++ [2, 3]
+  print $ length sample
+  print $ map (2*) sample
